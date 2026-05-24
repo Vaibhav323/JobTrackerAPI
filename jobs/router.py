ed2cfe222c1286191f15b3b6225aab6fb975b5aa
VAIBHAV_LONDHE\vmlon <vmlondhe25@gmail.com>
2026-05-24 22:38:57 +0530

    Partial Commit: Added Status endpoints with request response model

--- jobs/router.py
+++ jobs/router.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, HttpUrl
+from starlette import status
 from typing import Optional
 from enum import Enum
 import datetime as dt
@@ -45,6 +46,15 @@ class JobCreate(BaseModel):
     model_config = {"from_attributes": True}
 
 
+class FetchJob(BaseModel):
+    status: JobStatusType = None
+    company: str = None
+    job_type: JobType = None
+    search: str = None
+    page: int = 1
+    limit: int = 20
+
+
 class JobCreateResponse(BaseModel):
     user_id: str
     title: str
@@ -63,37 +73,101 @@ class JobCreateResponse(BaseModel):
     model_config = {"from_attributes": True}
 
 
-@router.post("/api/v1/jobs", tags=["Jobs"], response_model=JobCreateResponse)
+@router.post(
+    "/api/v1/jobs",
+    tags=["Jobs"],
+    response_model=JobCreateResponse,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_job(
     db: db_dependency, job_create_request: JobCreate, token=Depends(AuthDependency)
 ):
     initial_stauts = "WISHLIST" if not job_create_request.applied_at else "APPLIED"
     current_user_id = get_current_user(db=db, token=token)
-    new_job = Job(
-        user_id=current_user_id,
-        title=job_create_request.title,
-        company=job_create_request.company,
-        job_type=job_create_request.job_type,
-        url=str(job_create_request.url),
-        location=job_create_request.location,
-        salary_min=job_create_request.salary_min,
-        salary_max=job_create_request.salary_max,
-        notes=job_create_request.notes,
-        applied_at=job_create_request.applied_at,
-        current_status=initial_stauts,
-    )
-    db.add(new_job)
-    db.commit()
-    db.refresh(new_job)
+    try:
+        new_job = Job(
+            user_id=current_user_id,
+            title=job_create_request.title,
+            company=job_create_request.company,
+            job_type=job_create_request.job_type,
+            url=str(job_create_request.url),
+            location=job_create_request.location,
+            salary_min=job_create_request.salary_min,
+            salary_max=job_create_request.salary_max,
+            notes=job_create_request.notes,
+            applied_at=job_create_request.applied_at,
+            current_status=initial_stauts,
+        )
+        db.add(new_job)
 
-    new_status = StatusEvent(
-        job_id=new_job.id,
-        from_status=None,
-        to_status=initial_stauts,
-        changed_at=dt.datetime.now(dt.timezone.utc),
-    )
-    db.add(new_status)
-    db.commit()
+        new_status = StatusEvent(
+            job=new_job,
+            from_status=None,
+            to_status=initial_stauts,
+            changed_at=dt.datetime.now(dt.timezone.utc),
+        )
+        db.add(new_status)
+        db.commit()
+        db.refresh(new_job)
 
-    db.refresh(new_status)
-    return new_job
+        db.refresh(new_status)
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error: {e}"
+        )
+    else:
+        return new_job
+
+
+@router.get("/api/v1/jobs", tags=["Jobs"], status_code=status.HTTP_200_OK)
+async def fetch_jobs(
+    db: db_dependency, show_jobs: FetchJob, token=Depends(AuthDependency)
+):
+    pass
+
+
+@router.get("/api/v1/jobs/{job_id}", tags=["Jobs"], status_code=status.HTTP_200_OK)
+async def fetch_job(
+    db: db_dependency, token=Depends(AuthDependency), job_id: int = None
+):
+    pass
+
+
+@router.patch("/api/v1/jobs/{job_id}", tags=["Jobs"], status_code=status.HTTP_200_OK)
+async def update_job(
+    db: db_dependency, token=Depends(AuthDependency), job_id: int = None
+):
+    pass
+
+
+@router.delete(
+    "/api/v1/jobs/{job_id}", tags=["Jobs"], status_code=status.HTTP_204_NO_CONTENT
+)
+async def delete_job(
+    db: db_dependency, token=Depends(AuthDependency), job_id: int = None
+):
+    pass
+
+
+# Status Endpoints
+@router.post(
+    "/api/v1/jobs/{job_id}/status",
+    tags=["Status"],
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def create_status(
+    db: db_dependency, token=Depends(AuthDependency), job_id: int = None
+):
+    pass
+
+
+@router.post(
+    "/api/v1/jobs/{job_id}/timeline",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=["Status"],
+)
+async def create_timeline(
+    db: db_dependency, token=Depends(AuthDependency), job_id: int = None
+):
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth.router import router as auth_router
 from jobs.router import router as job_router
 from database import Base, engine
@@ -8,3 +9,14 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
 app.include_router(job_router)
+
+origins = [
+    "http://localhost:3000",  # React frontend
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
